15367468864c441b5c0b3e14f89d95a102d06537
박현수 <jadest@baghyeonsuui-MacBookPro.local>
2024-07-23 20:15:34 +0900

    netlify 설정_06

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="/space/edit" element={<SpaceEdit />}></Route>
+        <Route path="/edit" element={<SpaceEdit />}></Route>
       </Routes>
     </BrowserRouter>
     // <div className="App">
